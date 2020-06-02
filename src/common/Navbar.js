aa0cb7f8b9367a4ad0d81a3dbff6b2653de49bfd
joeyklee <joeyklee@nyu.edu>
2020-06-02 01:22:33 -0400

    adds style

--- src/common/Navbar.js
+++ src/common/Navbar.js
@@ -1,12 +1,18 @@
 const html = require('nanohtml');
 
+require('./Navbar.scss');
+
 const Navbar = () => {
 
   return html`
     <nav class="navbar">
-      <ul>
-        <li>Home</li>
-        <li>About</li>
+      <ul class="navbar__list">
+        <section class="navbar__list-section navbar__list-section--left">
+          <li class="navbar__list-item">BodyBlock</li>
+        </section>
+        <section class="navbar__list-section navbar__list-section--right">
+          “share the scene and not the rest 🙈”
+        </section>
       </ul>
     </nav>
   `
