8240376f1b7676068ec0401c10d2e8e733655ba8
Teghjyot <66196010+Teghjyot@users.noreply.github.com>
2020-06-04 12:32:04 +1000

    Added Unicode Hex for the emojis
    Sometimes emojis don't work so I added hex codes. I am new to GitHub and I know it's not important but, just trying, little-by-little.

--- src/common/Footer.js
+++ src/common/Footer.js
@@ -11,13 +11,13 @@ const Footer = () => {
           <li class="footer__list-item">In solidarity with #BLM</li>
         </section>
         <section class="footer__list-section footer__list-section--right">
-          Made with ❤ in Brooklyn
+          Made with &#x1F5A4 in Brooklyn
         </section>
       </ul>
     </footer>
   `
 }
 
-// “share the scene and not the rest 🙈”
+// “share the scene and not the rest &#x1f648;”
 
 module.exports = Footer;
